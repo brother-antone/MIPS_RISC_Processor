5f835db4aec61c56ae5d8f3b93b526b13750160d
brother-antone <lantonevich@yahoo.com>
2024-11-27 10:02:59 -0500

    Compiled Registers.vhd

--- Registers.vhd
+++ Registers.vhd
@@ -5,6 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity Registers is
 	Port(	
 		CLK				: in std_logic;
+		reset				: in std_logic;
 		RegW				: in std_logic;
 		SR1				: in std_logic_vector(4 downto 0);
 		SR2				: in std_logic_vector(4 downto 0);
@@ -16,9 +17,9 @@ entity Registers is
 end Registers;
 
 architecture Behavioral of Registers is
-	type register_32 is std_logic_vector(31 downto 0);
+	-- type register_32 is (std_logic_vector(31 downto 0));
 	
-	type registers is array (0 to 31) of register_32;
+	type registers is array (0 to 31) of std_logic_vector(31 downto 0);
 	
 	signal reg: registers:= (others => (others => '0'));
 	
@@ -26,9 +27,11 @@ begin
 	ReadReg1 <= reg(to_integer(unsigned(SR1)));
 	ReadReg2 <= reg(to_integer(unsigned(SR2)));
 	
-	process(CLK)
+	process(CLK, reset)
 	begin
-		if rising_edge(CLK) then
+		if reset = '1' then
+			reg <= (others => (others => '0'));
+		elsif rising_edge(CLK) then
 			if RegW = '1' then
 				reg(to_integer(unsigned(DR))) <= Reg_In;
 			end if;
